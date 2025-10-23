ca4570950096b0330ec4b4ee38bc15bb4f4fdac7
Sebxs940 <sprogramacion940@gmail.com>
2025-10-22 20:32:14 -0500

    Update project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import nodeAdapter from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(),],
+  output: 'server',         // importante para SSR
+  adapter: nodeAdapter(),    // adapter Node
+  integrations: [react()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
